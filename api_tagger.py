028e1016089a43b3e4a185251193998e7e5f6b8c
David <nundavid99@gmail.com>
2024-01-21 00:38:29 -0300

    provisional mojo, readme and api_tagger

--- api_tagger.py
+++ api_tagger.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import h2o
 
+from dncp import get_tender, get_planning, search_processes, get_release, get_releases, get_ocds_record
+from data import get_pd_dataframe
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -126,6 +129,27 @@ def api_predict():
     result.insert(2,'GRANDE',predClass.loc[0,'GRANDE'])
     
     return result.to_json(orient = "records")
+
+@app.route('/home', methods=['GET'])
+def api_home():
+    # tender_id = 0
+    id = request.args.get('id')
+    
+    record = get_ocds_record(id)
+    ocid = record['records'][0]['compiledRelease']['id']
+    release = get_release(ocid)
+    compiled_release = release['releases'][0]
+    df = get_pd_dataframe(compiled_release)
+    pred_class = h2o.mojo_predict_pandas(df, "mojo.zip", 
+                               genmodel_jar_path = 'jar/h2o-genmodel.jar', 
+                               classpath = 'jar/*')
+    pred_dict = pred_class.to_dict(orient = "records")[0]
+    return {
+        'prediction': pred_dict,
+        'compiledRelease': compiled_release,
+        'rows': 'df',
+    }
+    # return "<h1>Home</h1><p>API Tagger</p>"
 
 @app.errorhandler(404)
 def page_not_found(e):
